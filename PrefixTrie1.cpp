3b38339d4c3f740825134eb9ffc7b5958b140da2
Tanishq Baweja <tanishq.baweja001@gmail.com>
2022-06-24 18:39:04 +0530

    24/06/22->Modify Q1 of day 27

--- PrefixTrie1.cpp
+++ PrefixTrie1.cpp
@@ -59,3 +59,82 @@ public:
  * bool param_2 = obj->search(word);
  * bool param_3 = obj->startsWith(prefix);
  */
+
+// 24/06/22
+#include<bits/stdc++.h>
+/*
+    Your Trie object will be instantiated and called as such:
+    Trie* obj = new Trie();
+    obj->insert(word);
+    bool check2 = obj->search(word);
+    bool check3 = obj->startsWith(prefix);
+ */
+struct Node{
+    Node* links[26];
+    bool isTerminal = false;
+    
+    bool containsKey(char ch) {
+        return (links[ch - 'a'] != NULL);
+    }
+    
+    void put(char ch, Node* node) {
+        links[ch - 'a'] = node;
+    }
+    
+    Node* get(char ch) {
+        return links[ch - 'a'];
+    }
+    
+    void setEnd() {
+        isTerminal = true;
+    }
+    
+    bool isEnd() {
+        return isTerminal;
+    }
+    
+
+};
+
+class Trie {
+    Node* root;
+public:
+
+    /** Initialize your data structure here. */
+    Trie() {
+        root = new Node();
+    }
+
+    /** Inserts a word into the trie. */
+    void insert(string word) {
+        Node* temp = root;
+        for(char &ch : word) {
+            if(!temp->containsKey(ch)) {
+                temp->put(ch, new Node());
+            }
+            // moves to the reference node
+            temp = temp->get(ch);
+        }
+        temp->setEnd();
+    }
+
+    /** Returns if the word is in the trie. */
+    bool search(string word) {
+        Node* temp = root;
+        for(char &ch : word) {
+            if(!temp->containsKey(ch)) return false;
+            temp = temp->get(ch);
+        }
+        return temp->isEnd();
+    }
+
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    bool startsWith(string prefix) {
+        Node* temp = root;
+        for(char &ch : prefix) {
+            if(!temp->containsKey(ch)) return false;
+            temp = temp->get(ch);
+        }
+        return true;
+    }
+};
